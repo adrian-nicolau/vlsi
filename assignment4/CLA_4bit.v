b000758c144a08fca47e03c39215758450a84a67
Adrian Nicolau <adrian.nicolau74@gmail.com>
2014-05-18 12:57:48 +0300

    Module is final

--- assignment4/CLA_4bit.v
+++ assignment4/CLA_4bit.v
@@ -1,20 +1,49 @@
-module CLA_4bit(
-    output [3:0] S,
-    output Cout, PG, GG,
-    input [3:0] A, B,
-    input Cin
-    );
-    wire [3:0] G, P, C;
- 
-    assign G = A & B; //Generate
-    assign P = A ^ B; //Propagate
-    assign C[0] = Cin;
-    assign C[1] = G[0] | (P[0] & C[0]);
-    assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
-    assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
-    assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) |(P[3] & P[2] & P[1] & P[0] & C[0]);
-    assign S = P ^ C;
+module CLA_4bit(a0, a1, a2, a3, b0, b1, b2, b3, c_in, sum0, sum1, sum2, sum3, c_out);
 
-    assign PG = P[3] & P[2] & P[1] & P[0];
-    assign GG = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]);
+	input a0, a1, a2, a3, b0, b1, b2, b3, c_in;
+	output sum0, sum1, sum2, sum3, c_out;
+
+	wire p0, p1, p2, p3;
+	wire g0, g1, g2, g3;
+	wire c1, c2, c3, c4;
+	wire temp1_c1;
+	wire temp1_c2, temp2_c2;
+	wire temp1_c3, temp2_c3, temp3_c3;
+	wire temp1_c4, temp2_c4, temp3_c4, temp4_c4;
+
+	xor(p0, a0, b0);
+	xor(p1, a1, b1);
+	xor(p2, a2, b2);
+	xor(p3, a3, b3);
+
+	and(g0, a0, b0);
+	and(g1, a1, b1);
+	and(g2, a2, b2);
+	and(g3, a3, b3);
+
+	and(temp1_c1, p0, c_in);
+	or(c1, g0, temp1_c1);
+
+	and(temp1_c2, p1, g0);
+	and(temp2_c2, p1, p0, c_in);
+	or(c2, g1, temp1_c2, temp2_c2);
+
+	and(temp1_c3, p2, g1);
+	and(temp2_c3, p2, p1, g0);
+	and(temp3_c3, p2, p1, p0, c_in);
+	or(c3, g2, temp1_c3, temp2_c3, temp3_c3);
+
+	and(temp1_c4, p3, g2);
+	and(temp2_c4, p3, p2, g1);
+	and(temp3_c4, p3, p2, p1, g0);
+	and(temp4_c4, p3, p2, p1, p0, c_in);
+	or(c4, g3, temp1_c4, temp2_c4, temp3_c4, temp4_c4);
+
+	xor(sum0, p0, c_in);
+	xor(sum1, p1, c1);
+	xor(sum2, p2, c2);
+	xor(sum3, p3, c3);
+
+	or(c_out, c4, 0);
+
 endmodule
